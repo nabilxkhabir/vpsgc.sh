bba74ded00bab05ce17d6a992e4c5a13a6ac1bab
nabilxkhabir <37401854+nabilxkhabir@users.noreply.github.com>
2022-04-15 18:06:20 -0700

    Update GCngrok.sh

--- GCngrok.sh
+++ GCngrok.sh
@@ -3,7 +3,7 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: 27rEQ8MZ4tyIXjI4wtp2rFYpzXP_4j8Ki7Gnff3j3Bqsnqi9Q" CRP
+read -p "27rEQ8MZ4tyIXjI4wtp2rFYpzXP_4j8Ki7Gnff3j3Bqsnqi9Q" CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 echo "======================="
